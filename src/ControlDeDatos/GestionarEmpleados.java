19b2b9324f5e0809dbac27be4ba6131fa54ebf44
GuilleAQN <109323084+GuilleAQN@users.noreply.github.com>
2022-12-07 18:22:31 -0400

    Primer Commit

---
+++ src/ControlDeDatos/GestionarEmpleados.java
@@ -0,0 +1,18 @@
+package ControlDeDatos;
+
+import java.sql.*;
+
+public class GestionarEmpleados {
+
+    /*
+    Registrar Clases.Empleado
+    Modificar Clases.Empleado
+    Mostrar Clases.Empleado
+    Generar Volante
+    Editar Volante
+    Mostrar Volante
+    Finalizar Programa
+     */
+
+
+}
