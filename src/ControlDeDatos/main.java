322c58ea1661e40a089c9863b04216511b9ffe6f
Leandroreign <gonzalezleandro1903@gmail.com>
2022-12-11 15:50:22 -0800

    prueba

--- src/ControlDeDatos/main.java
+++ src/ControlDeDatos/main.java
@@ -7,11 +7,46 @@ package ControlDeDatos;
 
 // Interfaces importadas
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Scanner;
+
 
 public class main {
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Conexion cc = new Conexion();
-        Connection cn = cc.conectar();
+        try {
+            Conexion cc = new Conexion();
+            Connection cn = cc.conectar();
 
+            PreparedStatement ps = cn.prepareStatement("INSERT INTO empleados VALUES(?,?,?,?,?,?,?,?);");
+            
+            String id_empleado = sc.next;
+            ps.setString(1,id_empleado);
+
+            String nombre = sc.next;
+            ps.setString(2,nombre);
+
+            String apellido = sc.next;
+            ps.setString(3,apellido);
+
+            String cedula = sc.next;
+            ps.setString(4,cedula);
+
+            String direccion = sc.next;
+            ps.setString(5,direccion);
+
+            String rol = sc.next;
+            ps.setString(6,rol);
+
+            int estado = sc.next;
+            ps.setInt(7,estado);
+
+            Double Salario_bruto = sc.nextDouble;
+            ps.setDouble(8, Salario_bruto);
+
+        }catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE,null,ex);
+        }
     }// Cierre de Metodo
 }//Cierre de Clase
