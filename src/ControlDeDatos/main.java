c4d2405943115475b38a916c95d116c88f3e9080
Leandroreign <gonzalezleandro1903@gmail.com>
2022-12-11 16:02:04 -0800

    codigo comentado

--- src/ControlDeDatos/main.java
+++ src/ControlDeDatos/main.java
@@ -16,13 +16,13 @@ public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         try {
-            Conexion cc = new Conexion();
+            Conexion cc = new Conexion(); //Se crea un objeto de tipo connection con el fin de que los cambios realizados se realicen en la base de datos correspondiente
             Connection cn = cc.conectar();
 
-            PreparedStatement ps = cn.prepareStatement("INSERT INTO empleados VALUES(?,?,?,?,?,?,?,?);");
+            PreparedStatement ps = cn.prepareStatement("INSERT INTO empleados VALUES(?,?,?,?,?,?,?,?);"); //sentencia sql para añadira datos a la tabla empleados de la base de datos
             
-            String id_empleado = sc.next();
-            ps.setString(1,id_empleado);
+            String id_empleado = sc.next();//se utiliza el scanner para que el usuario ingrese el valor que desea
+            ps.setString(1,id_empleado);//posteriormente se coloca lo posicion y la variable con el valor introducido en la base de datos
 
             String nombre = sc.next();
             ps.setString(2,nombre);
@@ -43,12 +43,12 @@ public class main {
             ps.setInt(7,estado);
 
             Double Salario_bruto = sc.nextDouble();
-            ps.setDouble(8, Salario_bruto);
+            ps.setDouble(8, Salario_bruto); 
 
-            ps.executeUpdate();
+            ps.executeUpdate();//se ejecuta la actualizacion en la base de datos
 
         }catch (SQLException ex) {
-            Logger.getLogger(main.class.getName()).log(Level.SEVERE,null,ex);
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE,null,ex);// se captura este posible error en un catch
         }
     }// Cierre de Metodo
 }//Cierre de Clase
